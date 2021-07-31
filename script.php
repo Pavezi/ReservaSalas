b7b467c7ecc5acbb649780d2dc7ee5788c42882e
Carlos Schumacher <carloscreed@live.com>
2021-07-31 11:59:17 -0300

    adicionar imagem

--- script.php
+++ script.php
@@ -10,7 +10,7 @@ function gerarMenu() {
 function gerarLinhaCards() {
     var linha = ""
     for(i=0;i<4;i++){
-        linha = linha+"<section class=\"card_sala\"><p class=\"nome_sala\"> Sala 1 </p><p class=\"info_sala\">Sala 1 AMF 1</p> <hr> <input class=\"botao_mais\" type=\"button\" value=\"MAIS\"><input class=\"botao_reservar\" type=\"button\" value=\"RESERVAR\"> </section>"
+        linha = linha+"<section class=\"card_sala\"><p class=\"nome_sala\"><img src=\"chalkboard-teacher-solid.svg\" width=\"49\" alt=\"imagem\"> Sala 1 </p><p class=\"info_sala\">Sala 1 AMF 1</p> <hr> <input class=\"botao_mais\" type=\"button\" value=\"MAIS\"><input class=\"botao_reservar\" type=\"button\" value=\"RESERVAR\"> </section>"
         cards_gerados+=1
         if(cards_gerados==numero_salas){
             return linha
