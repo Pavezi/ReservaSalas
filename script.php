ad66b29ae048d2ecae7d90d2cf616a06b3b6466e
Carlos Schumacher <carloscreed@live.com>
2021-07-31 11:29:39 -0300

    edit

---
+++ script.php
@@ -0,0 +1,28 @@
+var numero_salas= 9
+var cards_gerados = 0
+var menu = document.getElementById('menu')
+function gerarMenu() {
+    do{
+        menu.innerHTML = menu.innerHTML+"<div class=\"indent-1\">"+gerarLinhaCards()+"</div>"
+    }while(cards_gerados<numero_salas)
+}
+
+function gerarLinhaCards() {
+    var linha = ""
+    for(i=0;i<4;i++){
+        linha = linha+"<section class=\"card_sala\"><p class=\"nome_sala\"> Sala 1 </p><p class=\"info_sala\">Sala 1 AMF 1</p> <hr> <input class=\"botao_mais\" type=\"button\" value=\"MAIS\"><input class=\"botao_reservar\" type=\"button\" value=\"RESERVAR\"> </section>"
+        cards_gerados+=1
+        if(cards_gerados==numero_salas){
+            return linha
+        }
+    }
+    return linha
+}
+
+function criarSala(){
+
+}
+
+function verificarCheckBox(){
+    
+}
